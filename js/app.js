80195e907919f665f015c9e601cb4f22c0e7d9a2
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-10-11 15:20:04 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator)
           {
           window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/service-worker.js')
+            navigator.serviceWorker.register('/js/service-worker.js')
             .then(function() { console.log("Service Worker Registered, Cheers to PWA Fire!"); });
           }
           );
